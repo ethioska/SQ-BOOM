4ba683c7df6f328461be3f8b92817b815ba09924
ethioska <ethioska@gmail.com>
2025-10-19 18:40:40 +0300

    Add Logo component to display 'SQ BOOM'

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="text-2xl font-semibold tracking-wide text-slate-900">
+      SQ BOOM
+    </div>
+  );
+}
